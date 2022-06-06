49bfadff811ad4b39c4e1e1c7370255c5adb72b5
svilen iordanov <svilenborianov@gmail.com>
2022-06-06 12:25:05 +0200

    Added race results

--- GtRacingNews/Controllers/RaceController.cs
+++ GtRacingNews/Controllers/RaceController.cs
@@ -4,9 +4,19 @@ namespace GtRacingNews.Controllers
 {
     public class RaceController : Controller
     {
-        public IActionResult Result(string Id)
+        public IActionResult Result(string championshipName)
         {
-            return View();
+            if (championshipName == "GT Europe Challenge By Fanatec")
+            return Redirect("https://www.gt-world-challenge-europe.com/results");
+
+            if (championshipName == "Super GT")
+                return Redirect("https://supergt.net/results?ln=en");
+
+            if (championshipName == "British GT")
+                return Redirect("https://www.britishgt.com/results");
+
+            else
+            return Redirect("https://www.gtopen.net/en/results/");
         }
     }
 }
