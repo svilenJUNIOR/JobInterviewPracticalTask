369cfcd9d62d780093246578a2c0d57d0c499217
svilen iordanov <svilenborianov@gmail.com>
2022-06-06 12:12:08 +0200

    Race controller

---
+++ GtRacingNews/Controllers/RaceController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GtRacingNews.Controllers
+{
+    public class RaceController : Controller
+    {
+        public IActionResult Result(string Id)
+        {
+            return View();
+        }
+    }
+}
